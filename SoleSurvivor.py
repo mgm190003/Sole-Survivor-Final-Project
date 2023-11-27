93eca12449b2f49fab057b51f821163026cb3f9c
mgm190003 <143027319+mgm190003@users.noreply.github.com>
2023-11-26 18:54:16 -0600

    Add files via upload

---
+++ SoleSurvivor.py
@@ -0,0 +1,20 @@
+import pygame
+import random
+
+def main():
+    #game setup
+    pygame.init
+    pygame.display.set_caption("Sole Survivor")
+    clock = pygame.time.Clock()
+    dt = 0
+    resolution = (1920, 1060)
+    screen = pygame.display.set_mode(resolution)
+    game = game(resolution)
+    running = True
+    #game loop
+    while running:
+        
+    pygame.quit
+    
+if __name__ == "__main__":
+    main()
