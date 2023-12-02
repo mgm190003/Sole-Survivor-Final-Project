a7495f78142870ca39c3eec1cf401c9162da4cd7
Max <mgm190003@utdallas.edu>
2023-12-01 19:59:36 -0600

    the player can now move

--- SoleSurvivor.py
+++ SoleSurvivor.py
@@ -2,31 +2,41 @@ import pygame
 import random
 
 class Player ():
-    def __init__(self, location=(0,0)):
-        self.location = location
+    def __init__(self, pos=(50,50), life=1000):
+        self.pos = pos
+        self.color = pygame.Color(255, 255, 255)
+        self.size = 50
         self.life = 3
+        self.speed = 0.2
+        self.dead = False
+        self.alpha = 255
+        self.surface = self.update_surface()
+        
+    def update(self):
+        if self.life < 1:
+            self.dead = True
+        #update player position
+        keys = pygame.key.get_pressed()
+        x=self.pos[0]
+        y=self.pos[1]
+        x += (keys[pygame.K_d] - keys[pygame.K_a]) * self.speed
+        y += (keys[pygame.K_s] - keys[pygame.K_w]) * self.speed
+        self.pos = [x,y]
+        
+            
 
-
-    def update_player(self):
-        #update position
-        events = pygame.event.get()
-        for event in events:
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    self.location[0] -= 1
-                if event.key == pygame.K_RIGHT:
-                    self.location[0] += 1
-                if event.key == pygame.K_DOWN:
-                    self.location[1] -= 1
-                if event.key == pygame.K_UP:
-                    self.location[1] += 1
-        self._draw_player()
-    
-    def _draw_player(self):
-        surf = pygame.Surface((25, 25))
-        surf.fill((0, 0, 0))
-        pygame.draw.rect(surf, (255, 255, 255), (25, 25, 25, 25))
+        
+    def update_surface(self):
+        surf = pygame.Surface((self.size, self.size))
+        surf.fill((255, 255, 255))
+        pygame.draw.rect(surf, self.color, (self.size, self.size, self.size, self.size))
         return surf
+    
+    def draw(self, surface):
+        if self.dead == True:
+            return
+        self.surface.set_alpha(self.alpha)
+        surface.blit(self.surface, self.pos)
         
         
 
@@ -42,16 +52,16 @@ class Game ():
     def __init__(self, screen_res):
         self.resolution = screen_res
         self.player = Player()
-    def update(self, dt):
-        self.player.update_player()
+    def update(self):
+        self.player.update()
+    def draw(self, surf):
+        self.player.draw(surf)
         
 
 def main():
     #game setup
     pygame.init()
     pygame.display.set_caption("Sole Survivor")
-    clock = pygame.time.Clock()
-    dt = 0
     resolution = (800, 600)
     screen = pygame.display.set_mode(resolution)
     game = Game(resolution)
@@ -63,12 +73,12 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
         # game logic
-        game.update(dt)
+        game.update()
         # render
         black = pygame.Color(0, 0, 0)
         screen.fill(black)
+        game.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(12)
     pygame.quit
     
 if __name__ == "__main__":
